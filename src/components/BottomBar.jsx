0ddb61f15aee6de0fb73f11b701ef1ceba9201d0
Thang Do <csessh@hey.com>
2025-12-17 15:34:04 +0700

    feat: add BottomBar component for mobile navigation

---
+++ src/components/BottomBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './BottomBar.module.css'
+
+export default function BottomBar({ canGoPrev, canGoNext, onPrev, onNext }) {
+  return (
+    <div className={styles.container}>
+      <button
+        className={styles.button}
+        onClick={onPrev}
+        disabled={!canGoPrev}
+      >
+        ◀ Prev
+      </button>
+      <button
+        className={styles.button}
+        onClick={onNext}
+        disabled={!canGoNext}
+      >
+        Next ▶
+      </button>
+    </div>
+  )
+}
