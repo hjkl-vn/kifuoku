d6eb8d28b698efdcb93294e6f7de3b146c4dfe36
Thang Do <tdo@hjkl.vn>
2026-01-12 14:58:48 +0700

    chore: add @testing-library/react for component testing

---
+++ vitest.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/setupTests.js']
+  },
+  resolve: {
+    alias: {
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils'
+    }
+  }
+})
